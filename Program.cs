7d8a94fbdc1f9b5202d53a2b1fab503f5729efec
hienquangtranht1 <hienquangtranht1@gmail.com>
2024-09-14 17:19:18 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(1, "Anh", 21);
+            Student s2 = new Student(2, "Hien", 20);
+            Student s3 = new Student(3, "Phap", 15);
+            Student s4 = new Student(4, "An", 18);
+            Student s5 = new Student(5, "Thien", 21);
+            List<Student> students = new List<Student> { s1, s2, s3, s4, s5 };
+
+
+            Console.WriteLine("Danh sach sinh vien:");
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+
+
+
+            Console.WriteLine("\n\nXuat danh sach tuoi tu 15 -> 18:");
+            var s = students.Where(x => x.Tuoi >= 15 && x.Tuoi <= 18).ToList();
+            foreach (var x in s)
+            {
+                Console.WriteLine(x.ToString());
+            }
+
+
+
+            Console.WriteLine("\n\nDanh sach bat dau tu chu A:");
+            var sa= students.Where(x => x.Hoten.StartsWith("A"));
+            foreach (var student in sa)
+            {
+                Console.WriteLine(student);
+            }
+
+
+           
+            int tongTuoi = students.Sum(x => x.Tuoi);
+            Console.WriteLine("\ntong so tuoi:"+tongTuoi);
+
+
+            Console.WriteLine("\n\nDS sinh vien co so tuoi lon nhat:");
+            var tuoiMax = students.Max(x => x.Tuoi);
+            var svTuoiMax = students.Where(x => x.Tuoi == tuoiMax);
+            foreach (var student in svTuoiMax)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("\n\nDS sap xep theo tuoi:");
+            var svsx = students.OrderBy(x => x.Tuoi);
+            foreach (var student in svsx)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace baitap
+{
+    class Student
+    {
+        private int mssv;
+        private string hoten;
+        private int tuoi;
+
+        public int Mssv { get => mssv; set => mssv = value; }
+        public string Hoten { get => hoten; set => hoten = value; }
+        public int Tuoi { get => tuoi; set => tuoi = value; }
+
+        public Student(int mssv, string hoten, int tuoi)
+        {
+            this.Mssv = mssv;
+            this.Hoten = hoten;
+            this.Tuoi = tuoi;
+        }
+
+       
+      
+
+        public override string ToString()
+        {
+            return $"MSSV: {mssv}, Ho Ten: {hoten}, Tuoi: {tuoi}";
+        }
+    }
+}
